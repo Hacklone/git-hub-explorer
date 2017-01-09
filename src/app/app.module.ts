92525070d0761e6e7796d34b0890f1f7a074382c
Barna Tóth <toakak@gmail.com>
2017-01-09 01:00:33 +0100

    feat(main): Add routing, Header and Search view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { HeaderComponent } from './header/header.component';
+import { AppRoutes } from "./app.routes";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+
+    AppRoutes
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { RouterModule, Routes } from "@angular/router";
+import { SearchComponent } from "./search/search.component";
+
+const routeConfigs: Routes = [
+  { path: '', component: SearchComponent },
+  { path: '**', redirectTo: 'search' }
+];
+
+export const AppRoutes = RouterModule.forRoot(routeConfigs);
